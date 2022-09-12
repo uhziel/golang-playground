fcc564a0fc78ffdf41b74255c0106adacf6b41bd
uhziel <uhziel@gmail.com>
2022-09-12 08:37:55 +0800

    试用 text/template

---
+++ cmd/ex2-go-template/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+    "log"
+    "os"
+    "text/template"
+)
+
+func main() {
+	const letter = `
+Dear {{.Name}},
+{{if .Attended}}
+It was a pleasure to see you at the wedding.
+{{- else}}
+It is a shame you couldn't make it to the wedding.
+{{- end}}
+{{with .Gift -}}
+Thank you for the lovely {{.}}.
+{{end}}
+Best wishes,
+Josie
+`
+
+	type Recipient struct {
+		Name, Gift string
+		Attended   bool
+	}
+	var recipients = []Recipient{
+		{"Aunt Mildred", "bone china tea set", true},
+		{"Uncle John", "moleskin pants", false},
+		{"Cousin Rodney", "", false},
+	}
+
+    t := template.Must(template.New("letter").Parse(letter))
+    for _, r := range recipients {
+        if err := t.Execute(os.Stdout, r); err != nil {
+            log.Fatalln(err)
+        }
+    }
+}
