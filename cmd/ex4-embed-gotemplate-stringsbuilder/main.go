a1b48d73383fae620697c251b3eb154cde41fdce
uhziel <uhziel@gmail.com>
2022-11-18 15:53:15 +0800

    使用 embed gotemplate strings builder 完成模版文件生成字符串

---
+++ cmd/ex4-embed-gotemplate-stringsbuilder/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+  "fmt"
+  "strings"
+  "text/template"
+  _ "embed"
+)
+
+type Person struct {
+  Name string
+}
+
+//go:embed hello.txt
+var hello string
+
+func main() {
+  tmpl, err := template.New("test").Parse(hello)
+  if err != nil {
+    panic(err)
+  }
+  lilei := Person{
+    Name: "lilei",
+  }
+  b := new(strings.Builder)
+  err = tmpl.Execute(b, lilei)
+  if err != nil {
+    panic(err)
+  }
+  fmt.Print(b.String())
+}
