0fa36508c88d11816b7946a24d7afa9c09a3a146
uhziel <uhziel@gmail.com>
2024-01-12 11:21:06 +0800

    试验 pipeline 模式

---
+++ patterns/ex5-channel-pipeline/main.go
@@ -0,0 +1,73 @@
+package main
+
+import "fmt"
+
+func toStream(done <-chan struct{}, nums []int) <-chan int {
+	stream := make(chan int)
+	go func() {
+		defer close(stream)
+
+		for _, num := range nums {
+			select {
+			case <-done:
+				return
+			case stream <- num:
+			}
+		}
+	}()
+	return stream
+}
+
+func add(done <-chan struct{}, n int, inStream <-chan int) <-chan int {
+	outStream := make(chan int)
+	go func() {
+		defer close(outStream)
+		for {
+			select {
+			case <-done:
+				return
+			case v, ok := <-inStream:
+				if !ok {
+					return
+				}
+				outStream <- v + n
+			}
+		}
+	}()
+	return outStream
+}
+
+func multiply(done <-chan struct{}, n int, inStream <-chan int) <-chan int {
+	outStream := make(chan int)
+	go func() {
+		defer close(outStream)
+		for {
+			select {
+			case <-done:
+				return
+			case v, ok := <-inStream:
+				if !ok {
+					return
+				}
+				outStream <- v * n
+			}
+		}
+	}()
+	return outStream
+}
+
+func main() {
+	nums := []int{
+		1,
+		3,
+		2,
+		10,
+	}
+
+	done := make(chan struct{})
+	defer close(done)
+	inCh := toStream(done, nums)
+	for v := range multiply(done, 2, add(done, 1, inCh)) {
+		fmt.Println(v)
+	}
+}
