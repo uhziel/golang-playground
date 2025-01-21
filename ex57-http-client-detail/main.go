79074d7fd0394281f9a34394f3a6fbc68310f142
uhziel <uhziel@gmail.com>
2025-01-21 11:09:16 +0800

    ex57-http-client-detail 构造自己的 Request

--- ex57-http-client-detail/main.go
+++ ex57-http-client-detail/main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"context"
 	"io"
 	"net/http"
 	"os"
 )
 
 func main() {
-	client := &http.Client{}
-	resp, err := client.Get("http://example.com")
+	ctx := context.Background()
+
+	client := &http.Client{} // 真实项目使用时可以使用 http.DefaultClient
+	req, err := http.NewRequestWithContext(ctx, "GET", "http://example.com", nil)
+	if err != nil {
+		panic(err)
+	}
+	resp, err := client.Do(req)
 	if err != nil {
 		panic(err)
 	}
