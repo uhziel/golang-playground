f5a38939874d007067c1e4d0ec3ccd22f462e9bc
uhziel <uhziel@gmail.com>
2025-01-21 11:58:41 +0800

    ex57-http-client-detail 构造自己的 Transport

--- ex57-http-client-detail/main.go
+++ ex57-http-client-detail/main.go
@@ -9,6 +9,8 @@ import (
 
 func main() {
 	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
 
 	client := &http.Client{} // 真实项目使用时可以使用 http.DefaultClient
 	req, err := http.NewRequestWithContext(ctx, "GET", "http://example.com", nil)
