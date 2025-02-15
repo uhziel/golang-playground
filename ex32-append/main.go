6cd5274d439a64656ea72ef5dac530c1f1b62465
uhziel <uhziel@gmail.com>
2025-02-15 22:53:44 +0800

    补全对 append() 的试验

--- ex32-append/main.go
+++ ex32-append/main.go
@@ -8,7 +8,7 @@ func appendInt(slice []int, elems ...int) []int {
 	n := len(slice)
 	m := n + len(elems)
 	if m > cap(slice) {
-		tmp := make([]int, cap(slice)*2)
+		tmp := make([]int, m*2)
 		copy(tmp, slice)
 		slice = tmp
 	}
@@ -21,7 +21,17 @@ func main() {
 	s := []int{1, 2, 3}
 	s = appendInt(s, 4, 5)
 	fmt.Println(s)
-	// 可以包含 nil...
+	s2 := appendInt(nil, s...)
+	fmt.Println(s2)
+
+	// func append(slice []Type, elems ...Type) []Type
+	// 参数 elems 可以是 nil
+	// 参数 slice 也可以是 nil
 	s = append(s, nil...)
 	fmt.Println(s)
+	var nilSlice []int
+	s3 := append(nilSlice, s...)
+	fmt.Println(s3)
+	s4 := append(nilSlice, nil...)
+	fmt.Println(s4)
 }
