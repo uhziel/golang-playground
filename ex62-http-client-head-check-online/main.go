613422bc1a9886e18432b13a54810236169778f2
uhziel <uhziel@gmail.com>
2025-03-20 11:31:48 +0800

    ex62-http-client-head-check-online: 添加超时、添加 tcp 验证方法

--- ex62-http-client-head-check-online/main.go
+++ ex62-http-client-head-check-online/main.go
@@ -3,18 +3,38 @@ package main
 import (
 	"fmt"
 	"io"
+	"net"
 	"net/http"
 	"os"
+	"time"
 )
 
 func main() {
-	resp, err := http.Head("http://localhost:6000/sync-to-s3")
+	head("http://localhost:6000/sync-to-s3")
+	tcpdial("localhost:6000")
+}
+
+func head(url string) {
+	client := &http.Client{
+		Timeout: 2 * time.Second,
+	}
+	resp, err := client.Head(url)
 	if err != nil {
-		fmt.Println("err", err)
+		fmt.Println("head: err", err)
 		return
 	}
 
-	fmt.Println(resp.Status)
-	fmt.Println(resp.Header)
+	fmt.Println("head:", resp.Status)
+	fmt.Println("head:", resp.Header)
 	io.Copy(os.Stdout, resp.Body)
+}
+
+func tcpdial(addr string) {
+	conn, err := net.DialTimeout("tcp", addr, 2 * time.Second)
+	if err != nil {
+		fmt.Println("tcpdial: err", err)
+		return
+	}
+	defer conn.Close()
+	fmt.Println("tcpdial: success")
 }
